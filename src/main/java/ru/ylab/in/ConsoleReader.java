e42b831b62878d5b6d2bc3cb4edd9d4478ac78eb
Evgeny Bayandin <5087081@gmail.com>
2024-06-26 13:39:04 +0300

    Revert "Add all files"
    This reverts commit 0724d3a681691e25baa2b86003997e3c8fab000b.

--- src/main/java/ru/ylab/in/ConsoleReader.java
+++
@@ -1,7 +0,0 @@
-package ru.ylab.in;
-
-public interface ConsoleReader {
-    String readLine();
-    int readInt();
-    Integer readIntSafely();
-}
--- src/main/java/ru/ylab/model/Resource.java
+++
@@ -1,9 +0,0 @@
-package ru.ylab.model;
-
-public interface Resource {
-    int getId();
-    String getName();
-    int getCapacity();
-    void setName(String name);
-    void setCapacity(int capacity);
-}
